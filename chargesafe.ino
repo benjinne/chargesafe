e60996d1709bf18188b06212a35eac19cba3e053
Travis Jones <tjones50@ycp.edu>
2017-10-21 16:08:02 -0400

    combined program

--- chargesafe.ino
+++ chargesafe.ino
@@ -1,35 +1,33 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include "Battery.h"
-#include "function.h"
+#include "getVoltages.h"
 
 // initialize the LCD library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-int buttonApin = 2;
-int buttonBpin = 3;
-bool buttonAValue;
-bool buttonBValue;
-int analogPin0 = 0;
+int donePin = 2;
+int nextPin = 3;
+bool done;
+bool next;
 
 const int ARRAYMAX = 10;
 int lastIndex;
 int displayIndex;
-double cells[4];
 Battery Batteries[ARRAYMAX];
+// Use one char and letter++?
+char letters[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'};
+int state;
 
 void setup() {
   Serial.begin(9600); 
   
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  pinMode(buttonApin, INPUT_PULLUP);  
-  pinMode(buttonBpin, INPUT_PULLUP);  
+  pinMode(donePin, INPUT_PULLUP);  
+  pinMode(nextPin, INPUT_PULLUP);  
   lastIndex = 0;
   displayIndex = 0;
-
-  addBattery('A', 5, 5, 5, 5);
-  addBattery('B', 2, 2, 2, 2);
-  addBattery('C', 4, 2, 2, 5);
+  state = 0;
 }
 
 void loop() {
@@ -38,56 +36,123 @@ void loop() {
   buttonHandler();
 
   // Display
-  displayBattery();
-  int val = analogRead(analogPin0);    // read the input pin
-  lcd.setCursor(0,1);
-  lcd.print(String(val));
+  view();
 }
 
-void addBattery(char Name, double Cell_1, double Cell_2 , double Cell_3, double Cell_4)
+void view(){
+  
+  if(state == 0){
+    lcd.setCursor(1, 0);
+    lcd.print("Welcome!" );
+    lcd.setCursor(1, 1);
+    lcd.print("Click Done..." );
+  }
+  else if(state == 1){
+    lcd.setCursor(0, 0);
+    lcd.print("Name = " + String(letters[lastIndex]) );
+    double arr[4];
+    double voltage = getVoltages(arr);
+    lcd.setCursor(0,1);
+    lcd.print(String(voltage) + " V");
+    lcd.setCursor(11,1);
+    lcd.print("Save?");
+  }
+  else if(state == 2){
+    // Display current battery
+    Battery thisBattery = Batteries[displayIndex];
+    lcd.setCursor(0, 0);
+    lcd.print("Name: " + thisBattery.getName() );
+    lcd.setCursor(0,1);
+    lcd.print(String(thisBattery.getTotal()) + "V");
+    lcd.setCursor(9,1);
+    lcd.print("Next ->");
+  }
+  else if(state == 3){
+    lcd.setCursor(1, 0);
+    lcd.print("Charging!" );
+    lcd.setCursor(1, 1);
+    lcd.print("Stuff" );
+  }
+  else if(state == 4){
+    lcd.setCursor(1, 0);
+    lcd.print("Finished!" );
+    lcd.setCursor(1, 1);
+    lcd.print("Restart?" );
+  }
+}
+
+void buttonHandler(){
+  // Done Button
+  if ( digitalRead(donePin) == LOW && digitalRead(donePin) != done )
+  {
+    done = digitalRead(donePin);
+    lcd.clear();
+    doneHandler();
+    delay(10);
+  }
+  else if (digitalRead(donePin) == HIGH && digitalRead(donePin) != done)
+  {
+    done = digitalRead(donePin);
+    lcd.clear();
+  }
+
+  // Next Button
+  if ( digitalRead(nextPin) == LOW && digitalRead(nextPin) != next )
+  {
+    next = digitalRead(nextPin);
+    lcd.clear();
+    nextHandler();
+    delay(10);
+  }
+  else if (digitalRead(nextPin) == HIGH && digitalRead(nextPin) != next)
+  {
+    next = digitalRead(nextPin);
+    lcd.clear();
+  }
+}
+
+void doneHandler(){
+  displayIndex = 0;
+  if(state + 1 <= 4){ state++; }
+  else{
+    state = 0; 
+    lastIndex = 0;
+    displayIndex = 0;
+  }
+}
+
+void nextHandler(){
+  if (state == 1){
+    if(lastIndex < ARRAYMAX){
+      double cells[4];
+      getVoltages(cells);
+      addBattery(lastIndex, cells[0], cells[1], cells[2], cells[3]);
+      popupMessage("Saved!", 1000);
+    }
+    else{
+      popupMessage("Not Saved, Full!", 1000);
+    }
+  }
+  else if(state == 2){
+    if(displayIndex < lastIndex-1){displayIndex++;}
+    else { displayIndex = 0; }
+  }
+}
+
+void addBattery(int letterIndex, double Cell_1, double Cell_2 , double Cell_3, double Cell_4)
 {
-  Batteries[lastIndex] = Battery(Name, Cell_1, Cell_2, Cell_3, Cell_4);
+  Batteries[lastIndex] = Battery(letters[letterIndex], Cell_1, Cell_2, Cell_3, Cell_4);
   lastIndex++;
 }
 
-void displayBattery(){
-  
-  test(cells);
-  Batteries[0] = Battery('A', cells[0], cells[1], cells[2], cells[3]);
-  
-  Battery thisBattery = Batteries[displayIndex];
+void popupMessage(String message, int delayMs){
+  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(thisBattery.getName() );
-  lcd.setCursor(1,0);
-  lcd.print(" = " + String(thisBattery.getTotal()));
+  lcd.print(message);
+  delay(delayMs);
+  lcd.clear();
 }
 
 
-void buttonHandler(){
- // Button A
-  if ( digitalRead(buttonApin) == LOW && digitalRead(buttonApin) != buttonAValue )
-  {
-    buttonAValue = digitalRead(buttonApin);
-    lcd.clear();
-    if(displayIndex + 1 < lastIndex){displayIndex++;}
-  }
-  else if (digitalRead(buttonApin) == HIGH && digitalRead(buttonApin) != buttonAValue)
-  {
-    buttonAValue = digitalRead(buttonApin);
-    lcd.clear();
-  }
 
-  // Button B
-  if ( digitalRead(buttonBpin) == LOW && digitalRead(buttonBpin) != buttonBValue )
-  {
-    buttonBValue = digitalRead(buttonBpin);
-    lcd.clear();
-    if(displayIndex - 1 >= 0){displayIndex--;}
-  }
-  else if (digitalRead(buttonBpin) == HIGH && digitalRead(buttonBpin) != buttonBValue)
-  {
-    buttonBValue = digitalRead(buttonBpin);
-    lcd.clear();
-  }
-}
 
--- getVoltages.h
+++ getVoltages.h
@@ -1,16 +1,18 @@
 #include "Arduino.h"
 
-int test(double * arr){
+double getVoltages(double * arr){
   unsigned long c1, c2, c3, c4;
   double v1, v2, v3, v4;
   double vpd = .0049; //volts per divison
   int numAvgs = 20;
+  
   for(int i=0; i<numAvgs; i++){
     c1 += analogRead(A0);
     c2 += analogRead(A1);
     c3 += analogRead(A2);
     c4 += analogRead(A3);
   }
+  
   c1 /= numAvgs;
   c2 /= numAvgs;
   c3 /= numAvgs;
@@ -18,12 +20,16 @@ int test(double * arr){
   
   v1 = (c1 - 3)*vpd;
   v2 = 2*(c2-7)*vpd - v1;
-  //v2 = 2*(c2-5)*vpd;
   v3 = 3*(c3-4)*vpd - v2 - v1;
-  //v3 = 3*(c3-4)*vpd;
   v4 = 4*(c4+10)*vpd - v3 - v2 - v1;
-  //v4 = 4*(c4+11)*vpd;
 
+  // FIX
+  arr[0] = 3; //v1;
+  arr[1] = 3; //v2;
+  arr[2] = 3; //v3;
+  arr[3] = 3; //v4;
+
+  
 
   Serial.print(v1);
   Serial.print("   -    ");
@@ -32,6 +38,10 @@ int test(double * arr){
   Serial.print(v3);
   Serial.print("   -    ");
   Serial.println(v4);
+
+  // FIX
+  //return v1+v3+v3+v4;
+  return 3+3+3+3;
       
 }
 
